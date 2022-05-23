34c2e4750c5ce9b2f34dd3b50d06640ad05fde81
Luftkatze <82577368+LuftKatzeBASIC@users.noreply.github.com>
2022-05-23 19:27:54 +0200

    Create lkfs.asm

---
+++ src/lkfs.asm
@@ -0,0 +1,6 @@
+db 'S'
+db "LKDOS",0,"              ",0 ;dir sys ro hidden
+db "IO.COM",0,"             ",0,0,1,1,1,0,18,4
+db "COMMAND.COM",0,"        ",0,0,1,1,1,0,13,5                  ; head (0), start(13), len (5)
+db 28
+times (0x1800-($-$$)) db 28
